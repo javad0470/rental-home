99469180077eab0708b9724997e47156e1320d93
javad <javad0470@gmail.com>
2025-04-16 12:26:18 +0330

    initial commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,15 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme:{
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns:{
+        '70/30': '70% 28%'
+      }
+    }
+  }
 };
 
 export default config;

3a47769d07fe79638ea8979fe1f45c5b71a6f0bb
javad <javad0470@gmail.com>
2025-04-18 21:53:52 +0330

    add mongodb and models

---
+++ models/Property.js
@@ -0,0 +1,72 @@
+import mongoose, {model, models, Schema} from "mongoose";
+
+const PropertySchema = new Schema({
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+        },
+        location: {
+            street: String,
+            city: String,
+            state: String,
+            zipcode: String
+        },
+        beds: {
+            type: Number,
+            required: true
+        },
+        bath: {
+            type: Number,
+            required: true
+        },
+        square_feet: {
+            type: Number,
+            required: true
+        },
+        amenities: [
+            {type: String}
+        ],
+        rates: {
+            nightly: Number,
+            weekly: Number,
+            monthly: Number,
+        },
+        seller_info: {
+            name: String,
+            email: String,
+            phone: String,
+        },
+        images: [
+            {type: String}
+        ],
+        is_featured: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Property = models.User || model("Property", PropertySchema);
+
+export default Property
+
+
+
+
+
+

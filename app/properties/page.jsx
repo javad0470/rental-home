c3f625925941a3265788d2c500ab8473057c5a66
javad <javad0470@gmail.com>
2025-04-18 23:26:02 +0330

    details screen

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,12 @@
-import properties from '@/properties.json'
 import PropertyCard from "@/components/PropertyCard";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
 
 
-export default function PropertiesPage() {
+export default async function PropertiesPage() {
+    await connectDB()
+    const properties = await Property.find({}).lean()
+
     return (
         <section className="px-4 py-6">
             <div className="container-xl lg:container m-auto px-4 py-6">
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,9 +1,15 @@
-import properties from '@/properties.json';
 import PropertyCard from "@/components/PropertyCard";
 import Link from "next/link";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
 
-export default function HomeProperties() {
-    const recentProperties = properties.slice(0, 3)
+export default async function HomeProperties() {
+    await connectDB()
+
+    const recentProperties = await Property.find({})
+        .sort({createdAt: -1})
+        .limit(3)
+        .lean()
 
     return (
         <>
@@ -12,7 +18,7 @@ export default function HomeProperties() {
                     <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
                         Recent Properties
                     </h2>
-                    {properties.length === 0 ? (
+                    {recentProperties.length === 0 ? (
                         <p>No Properties found</p>
                     ) : (
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
